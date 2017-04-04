a44d869118b12918a85e38570d6816f9aaa83634
John Furtado <jf8846@gmail.com>
2017-04-04 17:20:08 -0400

    Flicklist studio4

--- js/flicklist.js
+++ js/flicklist.js
@@ -8,13 +8,13 @@ var model = {
 
 var api = {
   root: "https://api.themoviedb.org/3",
-  token: "TODO", // TODO 0 add your api key
+  token: "", // TODO 0 done
   /**
    * Given a movie object, returns the url to its poster image
    */
   posterUrl: function(movie) {
     var baseImageUrl = "http://image.tmdb.org/t/p/w300/";
-    return baseImageUrl + movie.poster_path; 
+    return baseImageUrl + movie.poster_path;
   }
 }
 
@@ -26,11 +26,11 @@ var api = {
  * the callback function that was passed in
  */
 
-// TODO 1
+// TODO 1 done
 // this function should accept a second argument, `keywords`
-function discoverMovies(callback) {
+function discoverMovies(callback, keywords) {
 
-  // TODO 2 
+  // TODO 2 done
   // ask the API for movies related to the keywords that were passed in above
   // HINT: add another key/value pair to the `data` argument below
 
@@ -38,6 +38,7 @@ function discoverMovies(callback) {
     url: api.root + "/discover/movie",
     data: {
       api_key: api.token,
+      with_keywords: keywords
     },
     success: function(response) {
       model.browseItems = response.results;
@@ -48,59 +49,73 @@ function discoverMovies(callback) {
 
 
 /**
- * Makes an AJAX request to the /search/keywords endpoint of the API, using the 
+ * Makes an AJAX request to the /search/keywords endpoint of the API, using the
  * query string that was passed in
  *
  * if successful, invokes the supplied callback function, passing in
  * the API's response.
  */
 function searchMovies(query, callback) {
-  // TODO 3
+  // TODO 3 done
   // change the url so that we search for keywords, not movies
+  $.ajax({
+    url: api.root + "/search/keyword",
+    data: {
+      api_key: api.token,
+      query: query
+    },
+    success: function(response) {
+
+      console.log(response);
+
+      var keywordIDs = response.results.map( function (response) {
+          return response.id;
+      });
+
+      console.log(keywordIDs);
+
+      var keywordsString = keywordIDs.join('|');
+
+      console.log(keywordsString);
+
+      discoverMovies(render, keywordsString);
+
+
+
+    }
+  });
+}
 
 
   // TODO 4
   // when the response comes back, do all the tasks below:
 
 
-  // TODO 4a
+  // TODO 4a done
   // create a new variable called keywordIDs whose value is an array of all the
   // `.id` values of each of the objects inside reponse.results
   // HINT use the array map function to map over response.results
 
 
-  // TODO 4b
-  // create a new variable called keywordsString by converting 
+  // TODO 4b done
+  // create a new variable called keywordsString by converting
   // the array of ids to a comma-separated string, e.g.
   //      "192305,210090,210092,210093"
   // HINT: use the Array join function
 
 
-  // TODO 4c
+  // TODO 4c done
   // instead of a comma-separated string, we want the ids
   // to be spearated with the pipe "|" character, eg:
   //     "192305|210090|210092|210093"
   // HINT: pass an argument to the join function
 
 
-  // TODO 4d
-  // when the response comes back, call discoverMovies, 
+  // TODO 4d done
+  // when the response comes back, call discoverMovies,
   // passing along 2 arguments:
-  // 1) the callback 
+  // 1) the callback
   // 2) the string of keywords
-
-
-  $.ajax({
-    url: api.root + "/search/movie",
-    data: {
-      api_key: api.token,
-      query: query
-    },
-    success: function(response) {
-      console.log(response);
-    }
-  });
-}
 
 
 /**
@@ -115,7 +130,7 @@ function render() {
   // render watchlist items
   model.watchlistItems.forEach(function(movie) {
     var title = $("<h6></h6>").text(movie.original_title);
-      
+
     // movie poster
     var poster = $("<img></img>")
       .attr("src", api.posterUrl(movie))
@@ -135,7 +150,7 @@ function render() {
     var panelHeading = $("<div></div>")
       .attr("class", "panel-heading")
       .append(title);
-    
+
     // panel body contains the poster and button
     var panelBody = $("<div></div>")
       .attr("class", "panel-body")
@@ -168,7 +183,7 @@ function render() {
     var itemView = $("<li></li>")
       .attr("class", "list-group-item")
       .append( [title, overview, button] );
-      
+
     // append the itemView to the list
     $("#section-browse ul").append(itemView);
   });
